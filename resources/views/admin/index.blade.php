f7c261eb83b88faf779b95d6eb284dcb7e52a742
katherinekhine <khaingkatherine@gmail.com>
2024-07-18 21:02:13 +0630

    update index.blade.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,12 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>ADMIN</h1>
-</body>
-</html>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Admin Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="">
+                <div class="p-6 text-gray-900">
+                    {{-- {{ __("You're logged in!") }} --}}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
