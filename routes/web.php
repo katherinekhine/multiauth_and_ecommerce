c90e54cdf0699af0f20faaf27394eb98aed1dde0
katherinekhine <khaingkatherine@gmail.com>
2024-07-18 21:16:39 +0630

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__ . '/auth.php';
 
-Route::get('admin/dashboard', [HomeController::class, 'index']);
+Route::get('admin/dashboard', [HomeController::class, 'index'])->middleware(['auth', 'admin']);
