39e34517278cffccbda4a3241089c3c26c033dc7
katherinekhine <khaingkatherine@gmail.com>
2024-07-20 16:35:59 +0630

    update create.blade.php

--- resources/views/brand/create.blade.php
+++ resources/views/brand/create.blade.php
@@ -1,20 +1,5 @@
-{{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <div class="container">
-        <h1>Brand</h1>
-    </div>
-</body>
-</html> --}}
-@extends('layouts.app')
-@section('main')
+<x-app-layout>
     <div class="container">
         <h1>Brands</h1>
     </div>
-@endsection
+</x-app-layout>
