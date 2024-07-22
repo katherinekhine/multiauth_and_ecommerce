74eae004f1492a9cfd195c625d0b778efce0b1b2
katherinekhine <khaingkatherine@gmail.com>
2024-07-22 12:53:20 +0630

    update create.blade.php, app.css and index.blade.php

--- resources/views/brand/create.blade.php
+++ resources/views/brand/create.blade.php
@@ -1,5 +1,16 @@
 <x-app-layout>
-    <div class="container">
-        <h1>Brands</h1>
+    <div class="container pt-3 lg:px-40 md:px-6 px-5">
+        <h1 class="h1 mb-3">Create New Brand</h1>
+        <form action="" method="POST">
+            @csrf
+            <div class="mb-3">
+                <label for="name" class="font-medium block">Name:</label>
+                <input type="text" id="name" class="border w-full mt-1" placeholder="Enter Name...">
+            </div>
+            <div class="mb-3">
+                <a href="{{route('brands.index')}}" class="btn btn-back">Back</a>
+                <input type="submit" value="Create" class="btn">
+            </div>
+        </form>
     </div>
 </x-app-layout>
--- resources/views/brand/index.blade.php
+++ resources/views/brand/index.blade.php
@@ -1,5 +1,6 @@
 <x-app-layout>
     <div class="container pt-3 lg:px-40 md:px-6 px-5">
         <h1 class="h1">Brands</h1>
+        <a href="{{route('brands.create')}}">+ Create New Brand</a>
     </div>
 </x-app-layout>
