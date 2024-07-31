777bfbbfb12dcd09c5d5ed1c12465d182d2d3cc6
katherinekhine <khaingkatherine@gmail.com>
2024-07-31 21:05:05 +0630

    update BrandController.php

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -73,6 +73,7 @@ class BrandController extends Controller
      */
     public function destroy(Brand $brand)
     {
-        //
+        $brand->delete();
+        return redirect(route('brands.index'));
     }
 }
