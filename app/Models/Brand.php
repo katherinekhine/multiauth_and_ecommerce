c5acd29245b1ae177cfa7b9eb7181305f878f685
katherinekhine <khaingkatherine@gmail.com>
2024-08-02 21:47:50 +0630

    update Brand.php and Category.php

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -10,4 +10,9 @@ class Brand extends Model
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    public function category()
+    {
+        return $this->hasMany(Category::class);
+    }
 }
