8b3da07abdc9a4bb25f263eaade350a2f16e2c14
katherinekhine <khaingkatherine@gmail.com>
2024-08-14 21:22:40 +0630

    add available

---
+++ app/Models/Available.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Available extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['size', 'color', 'quantity', 'product_id'];
+}
