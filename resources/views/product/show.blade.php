bb0679c8ebfa3add96f8bff63c9e3a7b01690062
katherinekhine <khaingkatherine@gmail.com>
2024-08-15 21:48:38 +0630

    update show.blade.php of product

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="container py-3 lg:px-40 md:px-6 px-5">
         <h1 class="h1">Detail of Product</h1>
-        <div class="mt-3">
+        <div class="mt-3 mb-3">
             <div class="mb-3 font-bold">
                 <h2 class="text-xl"> <span>Name:</span>  {{ $product->name }}</h2>
                 <p class="text-sm mt-3"> <span>Description:</span> {{ $product->description }}</p>
@@ -11,5 +11,6 @@
             </div>
             <img src="{{ asset('storage/' . $product->photo) }}" alt="Product Image" class="w-48 h-48 object-cover rounded-lg">
         </div>
+        <a href="{{ route('products.index')}}" class="btn-back">Back</a>
     </div>
 </x-app-layout>
