a03a982312d767d5ee4b9e0762c1330826b291cf
Kei-K23 <keiksl2301@gmail.com>
2024-10-01 21:56:10 +0630

    Fix redirect from dashboard to home page

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -47,6 +47,7 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        // return redirect(route('dashboard', absolute: false));
+        return redirect('/');
     }
 }
